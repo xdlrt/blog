7af2108e39726e321885277c3c9372b27c1c1365
yeshu.lrt <yeshu.lrt@alibaba-inc.com>
2022-01-09 12:23:29 +0800

    refine: improve calendar injector

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,12 +1,9 @@
-hexo.config.calendar = Object.assign({
-  single: true,
-  root: 'calendar/'
-}, hexo.config.calendar);
+hexo.config.calendar = Object.assign({}, hexo.config.calendar);
 
 hexo.extend.generator.register('calendar', require('./lib/generator'));
 
 const version = '0.0.3';
 
-hexo.extend.injector.register('head_end', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/contribution-calendar@${version}/dist/embed.css /></link>`, 'default');
+hexo.extend.injector.register('head_end', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/contribution-calendar@${version}/dist/embed.css"></link>`, 'default');
 
 hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net/npm/contribution-calendar@${version}/dist/embed.js"></script>`, 'default');
--- scripts/lib/generator.js
+++ scripts/lib/generator.js
@@ -1,56 +1,27 @@
+// inspired by https://github.com/howiefh/hexo-generator-calendar
+
 'use strict';
 
 module.exports = function (locals) {
-  var config = this.config.calendar;
   var configRoot = this.config.root;
-  var single = config.single;
-  var calendarRoot = config.root;
 
-  function generateKey(date) {
-    var year = date.year();
-    var month = date.month() + 1; // month is started from 0
-    return year + '-' + month;
-  }
-
-  var posts = locals.posts.sort('date').filter(function (post) {
-    return post.published;
-  }).map(function (post) {
-    return {
-      title: post.title,
-      date: post.date,
-      link: configRoot + post.path
-    };
-  }).reduce(function (prev, cur, index, array) {
-    var key = generateKey(cur.date);
-
-    if (!prev.hasOwnProperty(key)) {
-      prev[key] = [];
-    }
-
-    prev[key].push(cur);
-    return prev;
-  }, {});
-
-  if (!single) {
-    if (calendarRoot[calendarRoot.length - 1] !== '/') {
-      calendarRoot += '/';
-    }
-
-    var keys = Object.keys(posts);
-    var calendars = keys.map(function (key) {
+  var posts = locals.posts.sort('date')
+    .filter(function (post) {
+      return post.published;
+    }).map(function (post) {
       return {
-        path: calendarRoot + key + '.json',
-        data: JSON.stringify(posts[key])
+        title: post.title,
+        date: post.date.format('YYYY-MM-DD'),
+        link: configRoot + post.path
       };
-    });
-
-    calendars.push({
-      path: calendarRoot + 'list.json',
-      data: JSON.stringify(keys)
-    });
-
-    return calendars;
-  }
+    }).reduce(function (prev, cur) {
+      var key = cur.date;
+      if (!prev.hasOwnProperty(key)) {
+        prev[key] = 0;
+      }
+      prev[key]++;
+      return prev;
+    }, {});
 
   return {
     path: 'calendar.json',
