205f38b52f6febb9e583c0da4acde73b8f5f716b
yeshu.lrt <yeshu.lrt@alibaba-inc.com>
2022-01-09 11:42:18 +0800

    refine: improve calendar injector

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -4,3 +4,9 @@ hexo.config.calendar = Object.assign({
 }, hexo.config.calendar);
 
 hexo.extend.generator.register('calendar', require('./lib/generator'));
+
+const version = '0.0.3';
+
+hexo.extend.injector.register('head_end', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/contribution-calendar@${version}/dist/embed.css /></link>`, 'default');
+
+hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net/npm/contribution-calendar@${version}/dist/embed.js"></script>`, 'default');
