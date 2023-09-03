3f11ba44694830bfe36ec67c8193f8dee840818a
yeshu <673643706@qq.com>
2023-09-03 19:45:18 +0800

    fix logo issue

--- src/config.ts
+++ src/config.ts
@@ -10,13 +10,6 @@ export const SITE = {
   postPerPage: 10,
 };
 
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
